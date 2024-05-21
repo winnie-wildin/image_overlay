2da1cb37a3996ad810f66ef9c4a1f94d343dfe3b
Shreya Kommuri <shreyakommuri2000@gmail.com>
2024-05-21 10:43:04 +0530

    m

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .main import apply_overlay_image
 WEB_DIRECTORY = "js"
 NODE_CLASS_MAPPINGS = {
 
-    "Image Overlay": apply_overlay_image,
+    "apply_overlay_image": apply_overlay_image,
 }
 CC_VERSION = 2.0
 
